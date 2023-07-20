8d0a51aa30a01636d711592a26709ea3802bbb91
billp <billpiazzetta@gmail.com>
2023-07-20 00:12:54 -0300

    Modifiquei o aplicativo para trabalhar todo com double

--- Program.cs
+++ Program.cs
@@ -4,52 +4,34 @@ namespace Calculadora
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int opcao = 10;
-            int a = 0;
-            int b = 0;
-            int resposta = 0;
-            double respostaDiv = 0.0;
-            bool escolhaDiv = false;
-
+        static void Main(string[] args) {
+            int opcao;
+            double a, b, resposta = 0.0;
+           
             opcao = menu();
-            while (opcao != 0) 
-            {
+            while (opcao != 0) {
                 a = lerNumero("Digite o primeiro número: ");
                 b = lerNumero("Digite o segundo número: ");
-                switch(opcao)
-                {
+                switch(opcao) {
                     case 1:
-                        escolhaDiv = false;
                         resposta = soma(a, b);
                         break;
                     case 2:
-                        escolhaDiv = false;
                         resposta = subtracao(a, b);
                         break;
                     case 3:
-                        escolhaDiv = false;
                         resposta = multiplicacao(a, b);
                         break;
                     case 4:
-                        escolhaDiv = true;
-                        respostaDiv = (double)divisao(a, b);
+                        resposta = (double)divisao(a, b);
                         break;
                 }
-                if (escolhaDiv)
-                {
-                    Console.WriteLine("Resposta = " + respostaDiv);
-                } else
-                {
                     Console.WriteLine("Resposta = " + resposta);
-                }
                 opcao = menu();
             }
         }
 
-        private static int menu()
-        {
+        private static int menu() {
             Console.WriteLine("");
             Console.WriteLine("### CALCULADORA ###");
             Console.WriteLine("-------------------");
@@ -64,75 +46,58 @@ namespace Calculadora
             bool opcaoValida = false;
             int opcaoMenu = -1;
 
-            while (!opcaoValida)
-            {
-                try
-                {
+            while (!opcaoValida) {
+                try {
                     opcaoMenu = int.Parse(Console.ReadLine());
-                    if (opcaoMenu >= 0 && opcaoMenu <= 4)
-                    {
+                    if (opcaoMenu >= 0 && opcaoMenu <= 4) {
                         opcaoValida = true;
                     }
-                }
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     Console.WriteLine("Opção inválida!");
                 }
             }
             return opcaoMenu;
         }
 
-        private static int lerNumero(string mensagem)
-        {
-            int numero = 0;
+        private static double lerNumero(string mensagem) {
+            double numero = 0.0;
             bool numeroValido = false;
 
-            while (!numeroValido)
-            {
-                try
-                {
+            while (!numeroValido) {
+                try {
                     Console.Write(mensagem);
-                    numero = int.Parse(Console.ReadLine());
+                    numero = double.Parse(Console.ReadLine());
                     numeroValido = true;
                 }
-                catch (Exception e)
-                {
+                catch (Exception e) {
                     Console.WriteLine("Não é um número, tente novamente");
                 }
             }
             return numero;
         }
 
-        private static int soma(int a, int b)
-        {
+        private static double soma(double a, double b) {
             return a + b;
         }
 
-        private static int subtracao(int a, int b)
-        {
+        private static double subtracao(double a, double b) {
             return a - b;
         }
 
-        private static int multiplicacao(int a, int b)
-        {
+        private static double multiplicacao(double a, double b) {
             return a * b;
         }
 
-        private static double divisao(int a, int b)
-        {
-            double aDouble = Convert.ToDouble(a);
-            double bDouble = Convert.ToDouble(b);
-
+        private static double divisao(double a, double b) {
+            double aDouble = a;
+            double bDouble = b;
             double resposta = 0.0;
-            try
-            {
-                if (bDouble != 0.0)
-                {
+            try {
+                if (bDouble != 0.0) {
                     resposta = aDouble / bDouble;
                 }
             }
-            catch
-            {
+            catch {
                 Console.WriteLine("Erro, divisão por zero!");
             }
             return resposta;
